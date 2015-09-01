cb14738387f4bda3e84c975bba2e190b3979d9a9
Rodrigo Morais <morais.rm@gmail.com>
2015-09-01 14:43:06 -0300

    Adding example and explanation about destruturing assignment.

--- app/variables/destructuring/destructuring.js
+++ app/variables/destructuring/destructuring.js
@@ -1,12 +1,49 @@
 'use strict';
 
 var destructuring_simple = function(){
-    let a, b;
-    [a, b] = [5, 6];
+    let [a, b] = [5, 6];
+
+    console.log(a);
+    console.log(b);
+
+    let [c,,d] = [1,2,3];
+
+    console.log(c);
+    console.log(d);
+};
+
+destructuring_simple();
+
+var array_return = function(){
+    return [5,6];
+};
+
+var destructuring_function = function(){
+    let [a, b] = array_return();
 
     console.log(a);
     console.log(b);
 };
 
-destructuring_simple();
+destructuring_function();
 
+var object_return = function(){
+    return {
+        firstName: 'Rodrigo',
+        lastName: 'Morais'
+    };
+};
+
+var destructuring_object = function(){
+    let {firstName: first_name, lastName: last_name} = object_return();
+
+    console.log(first_name);
+    console.log(last_name);
+
+    let {firstName, lastName} = object_return();
+
+    console.log(firstName);
+    console.log(lastName);
+};
+
+destructuring_object();
