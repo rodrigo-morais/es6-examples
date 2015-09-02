569698ba38e776dff92c8595af91b9d42e41db40
Rodrigo Morais <morais.rm@gmail.com>
2015-09-01 22:47:22 -0300

    Adding example and explanation about template strings.

---
+++ app/variables/templates/templates.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var template = function(x, y, z){
+    let color = "white";
+    let size = "big";
+
+    console.log("The cat is ${color} and ${size}.");
+    console.log(`The cat is ${color} and ${size}.`);
+};
+
+template();
