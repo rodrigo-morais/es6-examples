77223c467f74352af9205b8f686a9b624f536117
Rodrigo Morais <morais.rm@gmail.com>
2015-09-08 18:02:12 -0300

    Adding examples and explanations about new data structure Set.

---
+++ dist/app/array/set/set.js
@@ -0,0 +1,17 @@
+"use strict";
+
+var set1 = new Set();
+set1.add(1);
+set1.add(1);
+set1.add(2);
+set1.add(3);
+console.log(set1.size);
+
+set1["delete"](1);
+console.log(set1.size);
+
+set1.add(2);
+console.log(set1.size);
+
+set1.clear();
+console.log(set1.size);
