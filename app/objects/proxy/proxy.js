b28a8bf183023e8da5b914942df509eff85d2989
Rodrigo Morais <morais.rm@gmail.com>
2015-09-11 06:40:26 -0300

    Improving the Proxy example.

--- app/objects/proxy/proxy.js
+++ app/objects/proxy/proxy.js
@@ -3,7 +3,10 @@
 let soldier = {
     weapon: 'laser',
     allowed: 'to kill',
-    bullets: 0
+    bullets: 0,
+    attack(){
+        return 'kill people';
+    }
 };
 
 let proxySoldier = new Proxy(soldier, {
@@ -68,3 +71,20 @@ console.log(soldier.bullets);
 console.log('Proxy Soldier');
 console.log(proxySoldier.weapon);
 console.log(proxySoldier.bullets);
+
+soldier.attack = new Proxy(soldier.attack,{
+    apply(target, context, args){
+        if(context === 'pacifist'){
+            return "pacifists don't attack nobody";
+        }
+        else{
+            return target.apply(context, args);
+        }
+    }
+});
+
+let pacifist = {};
+pacifist.attack = soldier.attack;
+
+console.log(soldier.attack());
+console.log(pacifist.attack());
