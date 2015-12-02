a6bfc3bfa9fe79bdcd36900bd8fa45a1ace7b78d
Rodrigo Morais <morais.rm@gmail.com>
2015-12-01 22:34:50 -0200

    Creating an example to modules using the reserved word default.

---
+++ app/modules/default/employee_default.js
@@ -0,0 +1,13 @@
+export class Employee {
+	constructor(name) {
+		this._name = name;
+	}
+
+	get name() {
+		return this._name;
+	}
+
+	doWork() {
+		return '${this._name} is working';
+	}
+}
---
+++ app/modules/default/module.js
@@ -0,0 +1,4 @@
+import factory from './employee.js';
+
+var employee = new factory('Rodrigo');
+employee.doWork(); //Rodrigo is working
